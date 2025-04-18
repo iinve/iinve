b86a1ed054553463197400ecee622e0ef26d31b6
hananpk <hananpilakkadan@gmail.com>
2025-04-19 02:18:54 +0530

    tw-fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{ts,tsx}",
+    "./src/styles/**/*.css",
     "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
