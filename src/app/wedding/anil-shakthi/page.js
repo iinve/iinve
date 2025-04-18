ad2f99edf5f254d1d337a3c51ea09f9382f57bcd
hananpk <hananpilakkadan@gmail.com>
2025-04-18 23:43:20 +0530

    data addon

--- src/app/wedding/anil-shakthi/page.js
+++ src/app/wedding/anil-shakthi/page.js
@@ -1,31 +1,61 @@
-'use client'
-import MetaDataLayout from "@/Components/MetaDataLayout/MetaDataLayout";
-import { anilShakthiData } from "@/data/Anil-Shakthi";
 import dynamic from "next/dynamic";
+import Head from "next/head";
 
+// Determine the base URL of your application
+const isProduction = process.env.NODE_ENV === "production";
+const metadataBase = isProduction
+  ? "https://iinve.com"
+  : "http://localhost:3000";
 
-const metadata = {
+// Dynamically import the data module
+const AnilShakthiData = dynamic(() => import('@/data/Anil-Shakthi'), {
+  ssr: false, // Make sure it only loads on the client side
+});
+
+export const metadata = {
   title: "Anil & Shakthi Wedding Invitation | iinve",
   description:
     "Anil and Shakthi are getting married and joyfully invite you to celebrate their wedding.",
-  ogImageUrl: "/coffeePremium/anil_og_image.jpg", // Relative path for the Open Graph image
-  ogAlt: "Anil & Shakthi Wedding Invitation",
+  openGraph: {
+    title: "Anil & Shakthi Wedding Invitation | iinve",
+    description:
+      "Anil and Shakthi are getting married and joyfully invite you to celebrate their wedding.",
+    images: [
+      {
+        url: `${metadataBase}/coffeePremium/anil_og_image.jpg`,
+        alt: "Anil & Shakthi Wedding Invitation",
+      },
+    ],
+  },
+  metadataBase: metadataBase, // Add this line to set the metadataBase
 };
 
 const CoffeePremium = dynamic(() => import("@/Templates/CoffeePremium"), {
-  ssr: false,
+  ssr: false, // Ensure it only loads on the client side
 });
 
 const Page = () => {
   return (
     <div>
-      <MetaDataLayout
-        title={metadata.title}
-        description={metadata.description}
-        ogImageUrl={metadata.ogImageUrl}
-        ogAlt={metadata.ogAlt}
-      />
-      <CoffeePremium data={anilShakthiData} />
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        {metadata.openGraph.images[0].alt && (
+          <meta
+            property="og:image:alt"
+            content={metadata.openGraph.images[0].alt}
+          />
+        )}
+      </Head>
+      <AnilShakthiData>
+        {(data) => <CoffeePremium data={data.anilShakthiData} />}
+      </AnilShakthiData>
     </div>
   );
 };
