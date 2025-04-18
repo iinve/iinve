c4bcb2f9c9cdf1c299c25ece7585e3e42e84c740
hananpk <hananpilakkadan@gmail.com>
2025-04-19 03:36:50 +0530

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,14 @@
-
-
+// src/app/layout.tsx
 import { HeroUIProvider } from "@heroui/react";
 import ClientLayout from "Components/ClientLayout/ClientLayout";
 import MainLayout from "Components/MainLayout/MainLayout";
-import { DM_Sans, Josefin_Sans } from "next/font/google";
-import Head from "next/head";
-import '../styles/global.css';
+import { Josefin_Sans } from "next/font/google";
+import "../styles/global.css";
 
-
-
-const DMSans = DM_Sans({ subsets: ["latin"] });
+// Setup font
 const JosefinSans = Josefin_Sans({ subsets: ["latin"] });
 
-const isProduction = process.env.NODE_ENV === "production";
-const metadataBase = isProduction
-  ? "https://iinve.com"
-  : "http://localhost:3000";
-
+// Metadata config for Next.js App Router
 export const metadata = {
   title: "iinve - Create Magic for You",
   description:
@@ -27,45 +19,23 @@ export const metadata = {
       "Plan your perfect wedding with iinve's easy-to-use invitations. Send stunning invitations with your favorite images, and share important details with your loved ones. Start creating your dream wedding website today with iinve!",
     images: [
       {
-        url: `${metadataBase}/assets/images/og-image.jpg`,
+        url: "https://iinve.com/assets/images/og-image.jpg",
         alt: "iinve - Create Magic for You",
       },
     ],
   },
-  metadataBase: metadataBase,
 };
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.openGraph.title} />
-        <meta
-          property="og:description"
-          content={metadata.openGraph.description}
-        />
-        <meta property="og:image" content={metadata.openGraph.images[0].url} />
-
-        {metadata.openGraph.images[0].alt && (
-          <meta
-            property="og:image:alt"
-            content={metadata.openGraph.images[0].alt}
-          />
-        )}
-      </Head>
-      <html lang="en">
-        <body className={JosefinSans.className}>
-          <MainLayout>
-            <ClientLayout>
-              <HeroUIProvider>
-                {children}
-              </HeroUIProvider>
-            </ClientLayout>
-          </MainLayout>
-        </body>
-      </html>
-    </>
+    <html lang="en">
+      <body className={JosefinSans.className}>
+        <MainLayout>
+          <ClientLayout>
+            <HeroUIProvider>{children}</HeroUIProvider>
+          </ClientLayout>
+        </MainLayout>
+      </body>
+    </html>
   );
 }
