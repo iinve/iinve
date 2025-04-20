d1b4378503afd1697242b51276a4acd77c4d0e58
hananpk <hananpilakkadan@gmail.com>
2025-04-20 18:57:03 +0530

    offer fix

--- src/Components/DigitalWall/Galaxy/Banner/Banner.jsx
+++ src/Components/DigitalWall/Galaxy/Banner/Banner.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import ReactPlayer from "react-player/vimeo";
 import Style from "./Banner.module.scss";
 
 const Banner = ({ data }) => {
-
   return (
     <div className={Style.banner}>
-      {data?.backgroundVideo && (
-        <video
-          className={Style.video}
-          autoPlay
-          loop
-          muted
-          playsInline
-          width={100}
-          height={100}
-          src={data.backgroundVideo}
-        />
-      )}
+      <ReactPlayer
+        url="https://player.vimeo.com/video/1077063863?h=5043d53d91"
+        playing
+        muted
+        loop
+        playsinline
+        controls={false}
+        width="100%"
+        height="100%"
+        className={Style.video}
+        config={{
+          vimeo: {
+            playerOptions: {
+              title: 0,
+              byline: 0,
+              portrait: 0,
+              autopause: 0,
+            },
+          },
+        }}
+      />
       <h5 dangerouslySetInnerHTML={{ __html: data?.subHeading }}></h5>
     </div>
   );
