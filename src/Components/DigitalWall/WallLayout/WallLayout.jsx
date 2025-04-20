6f2400bac8a3757e5b88db2b8429e8ec5b42f2d0
hananpk <hananpilakkadan@gmail.com>
2025-04-20 11:14:54 +0530

    animation add

--- src/Components/DigitalWall/WallLayout/WallLayout.jsx
+++ src/Components/DigitalWall/WallLayout/WallLayout.jsx
@@ -1,23 +1,43 @@
-'use client'
-import { WallLogo } from '../WallLogo/WallLogo'
-import Style from './WallLayout.module.scss'
+'use client';
+import { useEffect, useState } from 'react';
+import { WallLogo } from '../WallLogo/WallLogo';
+import Style from './WallLayout.module.scss';
 
 const WallLayout = ({ children, background }) => {
+  const [showUI, setShowUI] = useState(true);
+  let scrollTimeout;
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowUI(false);
+
+      clearTimeout(scrollTimeout);
+      scrollTimeout = setTimeout(() => {
+        setShowUI(true);
+      }, 200); 
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      clearTimeout(scrollTimeout);
+    };
+  }, []);
+
   return (
     <div className={Style.WallLayout} style={{ '--backgroundColor': background }}>
-      <div className={Style.badge}>
+      <div className={`${Style.badge} ${!showUI ? Style.hidden : ''}`}>
         <WallLogo width={120} height={120} />
       </div>
+
       {children}
 
-      {/* footer drawer */}
-      <div className={Style.footer}>
-        {/* <WallLogo width={120} height={120} /> */}
+      <div className={`${Style.footer} ${!showUI ? Style.hidden : ''}`}>
         <h1 className='text-black'>Logo of the company</h1>
         <button>Contact us</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default WallLayout
+export default WallLayout;
