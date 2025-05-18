1b8befe66f2eaa5d04392b49ad521b29ea768db7
dilshanmambadan <dilshanmambadan@gmail.com>
2025-05-18 15:50:11 +0530

    create mushida wedding iinvite

---
+++ src/DB/Ashmil-Munshida.js
@@ -0,0 +1,54 @@
+import Bride from "../assets/coffeePremium/Ashmil-Munshida/bride.jpg";
+import image01 from "../assets/coffeePremium/Ashmil-Munshida/image01.jpg";
+import image02 from "../assets/coffeePremium/Ashmil-Munshida/image02.jpg";
+import image03 from "../assets/coffeePremium/Ashmil-Munshida/image03.jpg";
+import Groom from "../assets/coffeePremium/Ashmil-Munshida/groom.jpg";
+import image04 from "../assets/coffeePremium/Ashmil-Munshida/image04.jpg";
+import image05 from "../assets/coffeePremium/Ashmil-Munshida/image05.jpg";
+import image06 from "../assets/coffeePremium/Karthik-Varsha/six.jpeg";
+
+export const ashmil_munshida = {
+  bride: "Munshida",
+  groom: "Ashmil",
+  phone1: "",
+  theme: "#3c4028",
+  default_color: "#FEF3E2",
+  highlight_color: "#ddae6b",
+  phone2: "",
+  begin_time: "11:30 AM",
+  music: "/audio/christina.mp3",
+  quote:
+    "invite you to join us as we celebrate the beginning of our forever. Surrounded by the blessings of family, friends, and well-wishers, we step into a new chapter of life together and your presence will make our day truly complete.",
+  hide_info: false,
+  couples_data: [
+    {
+      full_name: "Ashmil",
+      bio: "S/O   Ali Asker and Febina",
+      avatar: Groom,
+    },
+    {
+      full_name: "Munshida K P",
+      bio: "D/O Sakeer and Shameena",
+      avatar: Bride,
+    },
+  ],
+
+  images: [image01, image02, image03, image04, image01, image05, image06],
+  dateData: [
+    { date: "22", day: "Thu" },
+    { date: "23", day: "Fri" },
+    { date: "24", day: "Sat" },
+    { date: "25", day: "Sun", active: true },
+    { date: "26", day: "Mon" },
+    { date: "27", day: "Tue" },
+    { date: "28", day: "Wed" },
+  ],
+  month: "May",
+  //   muhurtham: "11:50 AM",
+  venue: "Home",
+  place: "Trissur",
+  date: "2025-05-25",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d121935.68910295404!2d76.14067672540797!3d10.511376927167735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7ee15ed42d1bb%3A0x82e45aa016ca7db!2sThrissur%2C%20Kerala!5e1!3m2!1sen!2sin!4v1746355211752!5m2!1sen!2sin",
+  // map_name: "Kalikavu",
+  map_link: "https://maps.app.goo.gl/x28DkprL52KccaUF8",
+};
