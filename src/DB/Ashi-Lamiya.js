cf2c50286403e882237a37a9a1b2196797c9eecc
dilshanmambadan <dilshanmambadan@gmail.com>
2025-05-26 21:42:29 +0530

    ashi-lamiya completed

---
+++ src/DB/Ashi-Lamiya.js
@@ -0,0 +1,67 @@
+import Bride from "../assets/coffeePremium/Ashi-Lami/bride.png";
+import image01 from "../assets/coffeePremium/Ashi-Lami/ashi-lami1.jpg";
+import image02 from "../assets/coffeePremium/Ashi-Lami/ashi-lami2.jpg";
+import image03 from "../assets/coffeePremium/Ashi-Lami/ashi-lami3.jpg";
+import Groom from "../assets/coffeePremium/Ashi-Lami/groom.png";
+import image04 from "../assets/coffeePremium/Ashi-Lami/ashi-lami4.jpg";
+import image05 from "../assets/coffeePremium/Ashi-Lami/ashi-lami5.jpg";
+import image06 from "../assets/coffeePremium/Ashi-Lami/ashi-lami6.jpg";
+import image07 from "../assets/coffeePremium/Ashi-Lami/ashi-lami7.jpg";
+import image08 from "../assets/coffeePremium/Ashi-Lami/ashi-lami8.jpg";
+
+export const ashi_lamiya = {
+  bride: "Lamiya",
+  groom: "Ashi",
+  phone1: "",
+  theme: "#3c4028",
+  default_color: "#FEF3E2",
+  send_wishes: true,
+  event_type: "wedding",
+  highlight_color: "#ddae6b",
+  phone2: "",
+  begin_time: "11:30 AM",
+  music: "/audio/audio1.mp3",
+  quote:
+    "invite you to join us as we celebrate the beginning of our forever. Surrounded by the blessings of family, friends, and well-wishers, we step into a new chapter of life together and your presence will make our day truly complete.",
+  hide_info: false,
+  couples_data: [
+    {
+      full_name: "Ashi",
+      bio: "S/O  Father name",
+      avatar: Groom,
+    },
+    {
+      full_name: "Lamiya",
+      bio: "D/O Father name",
+      avatar: Bride,
+    },
+  ],
+
+  images: [
+    image01,
+    image02,
+    image03,
+    image04,
+    image05,
+    image06,
+    image07,
+    image08,
+  ],
+  dateData: [
+    { date: "18", day: "Thu" },
+    { date: "19", day: "Fri" },
+    { date: "20", day: "Sat" },
+    { date: "21", day: "Sun", active: true },
+    { date: "22", day: "Mon" },
+    { date: "23", day: "Tue" },
+    { date: "24", day: "Wed" },
+  ],
+  month: "Sep",
+  //   muhurtham: "11:50 AM",
+  venue: "Golden Valley auditorium  ",
+  place: "Vaniyambalam",
+  date: "2025-09-21",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3913.9752048085784!2d76.26195401046823!3d11.189470288939573!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba6252ca6bd5cc7%3A0x5ab90cab97386561!2sGolden%20Valley%20Auditorium!5e0!3m2!1sen!2sin!4v1748275273827!5m2!1sen!2sin",
+  // map_name: "Kalikavu",
+  map_link: "https://maps.app.goo.gl/5Jjd9q68zgKAoxxm7?g_st=iw",
+};
