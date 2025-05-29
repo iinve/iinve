16612e8a5bebb7b3c052d34c130fef238e04620d
dilshanmambadan <dilshanmambadan@gmail.com>
2025-05-30 00:13:31 +0530

    pattholic design

--- src/Components/Editor/ThemeSelector/ThemeSelector.jsx
+++ src/Components/Editor/ThemeSelector/ThemeSelector.jsx
@@ -1,148 +1,185 @@
-import { addToast, Button, Input, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader } from "@heroui/react";
+import {
+  addToast,
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@heroui/react";
 import ProIcon from "ProUI/Icons/icons";
 import { useEffect, useState } from "react";
 import { HexColorPicker } from "react-colorful";
 
-const ThemeSelector = ({ colors,setAllColors, selectedTheme, selectedHighlightedColor, selectedContentColor, selectedThemeColor, handleChooseThemeColor }) => {
-  const [isOpen, setIsOpen] = useState(false)
+const ThemeSelector = ({
+  colors,
+  setAllColors,
+  selectedTheme,
+  selectedHighlightedColor,
+  selectedContentColor,
+  selectedThemeColor,
+  handleChooseThemeColor,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
   const [color, setColor] = useState("##FF0000");
 
   const handleOpenMoreColorModal = () => {
-    setIsOpen(!isOpen)
-  }
+    setIsOpen(!isOpen);
+  };
   const handleAddColor = () => {
     if (!colors?.some((c) => c.hex === color)) {
-      colors.push({ hex: color })
+      colors.push({ hex: color });
       handleOpenMoreColorModal();
     } else {
       addToast({
-        title: 'Oops!',
+        title: "Oops!",
         description: `This color is already added!`,
-        type: '',
-        color: 'warning',
-        variant: 'flat',
+        type: "",
+        color: "warning",
+        variant: "flat",
       });
     }
   };
-  
-  return <>
-    <div className={`p-8 border border-dashed border-[#333] rounded-2xl mb-6 w-full`}>
-      <div className='flex items-center justify-between'>
-        <div>
-          <h2 className='text-lg'>Theme Color</h2>
-          {!colors?.length && <p className='text-[#333]'>Please upload logo for selecting theme color.</p>}
-        </div>
-        <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
-          {colors
-            ?.filter(
-              (item) =>
-                item.hex !== selectedContentColor?.hex
-            )
-            .map((color, idx) => (
-              <div
-                key={`color_${idx}`}
-                className={`border-2 ${color?.hex === selectedTheme?.theme_color ? 'border-blue-600' : 'border-gray-300'} p-1 rounded-2xl`}
-              >
-                <span
-                  key={color.hex}
-                  className={`w-[50px] h-[50px] block rounded-xl`}
-                  style={{ backgroundColor: color?.hex }}
-                  onClick={() => handleChooseThemeColor(color, 'theme')}
-                ></span>
-              </div>
-            ))}
 
-        </div>
-
-      </div>
-
-      {/* content color selection */}
-
-      <div className="">
-        {colors?.length > 0 && (
-          <div className='flex items-center justify-between mt-10'>
-            <div>
-              <h2>Text Color</h2>
-            </div>
-            <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
-              {colors
-                ?.filter(
-                  (item) =>
-                    item.hex !== selectedThemeColor?.hex
-                )
-                .map((color, idx) => (
-                  <div
-                    key={`color_${idx}`}
-                    className={`border-2 ${color?.hex === selectedTheme?.content_color ? 'border-blue-600' : 'border-gray-300'} p-1 rounded-2xl`}
-                  >
-                    <span
-                      key={color.hex}
-                      className={`w-[50px] h-[50px] block rounded-xl`}
-                      style={{ backgroundColor: color?.hex }}
-                      onClick={() => handleChooseThemeColor(color, 'content')}
-                    ></span>
-                  </div>
-                ))}
-            </div>
+  return (
+    <>
+      <div
+        className={`p-8 border border-dashed border-[#333] rounded-2xl mb-6 w-full`}
+      >
+        <div className="flex items-center justify-between">
+          <div>
+            <h2 className="text-lg">Theme Color</h2>
+            {!colors?.length && (
+              <p className="text-[#333]">
+                Please upload logo for selecting theme color.
+              </p>
+            )}
           </div>
-        )}
-
-        {/* highlighted color */}
-        {colors?.length > 0 && (
-          <div className='flex items-center justify-between mt-10'>
-            <div>
-              <h2>Highlight Color</h2>
-            </div>
-            <div className='grid grid-cols-2 md:grid-cols-4 gap-2'>
-              {colors
-                ?.filter(
-                  (item) =>
-                    item.hex !== selectedThemeColor?.hex
-                )
-                .map((color, idx) => (
-                  <div
-                    key={`color_${idx}`}
-                    className={`border-2 ${color?.hex === selectedTheme?.highlight_color ? 'border-blue-600' : 'border-gray-300'} p-1 rounded-2xl`}
-                  >
-                    <span
-                      key={color.hex}
-                      className={`w-[50px] h-[50px] block rounded-xl`}
-                      style={{ backgroundColor: color?.hex }}
-                      onClick={() => handleChooseThemeColor(color, 'highlight')}
-                    ></span>
-                  </div>
-                ))}
-            </div>
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+            {colors
+              ?.filter((item) => item.hex !== selectedContentColor?.hex)
+              .map((color, idx) => (
+                <div
+                  key={`color_${idx}`}
+                  className={`border-2 ${
+                    color?.hex === selectedTheme?.theme_color
+                      ? "border-blue-600"
+                      : "border-gray-300"
+                  } p-1 rounded-2xl`}
+                >
+                  <span
+                    key={color.hex}
+                    className={`w-[50px] h-[50px] block rounded-xl`}
+                    style={{ backgroundColor: color?.hex }}
+                    onClick={() => handleChooseThemeColor(color, "theme")}
+                  ></span>
+                </div>
+              ))}
           </div>
+        </div>
+
+        {/* content color selection */}
+
+        <div className="">
+          {colors?.length > 0 && (
+            <div className="flex items-center justify-between mt-10">
+              <div>
+                <h2>Text Color</h2>
+              </div>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+                {colors
+                  ?.filter((item) => item.hex !== selectedThemeColor?.hex)
+                  .map((color, idx) => (
+                    <div
+                      key={`color_${idx}`}
+                      className={`border-2 ${
+                        color?.hex === selectedTheme?.content_color
+                          ? "border-blue-600"
+                          : "border-gray-300"
+                      } p-1 rounded-2xl`}
+                    >
+                      <span
+                        key={color.hex}
+                        className={`w-[50px] h-[50px] block rounded-xl`}
+                        style={{ backgroundColor: color?.hex }}
+                        onClick={() => handleChooseThemeColor(color, "content")}
+                      ></span>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          )}
+
+          {/* highlighted color */}
+          {colors?.length > 0 && (
+            <div className="flex items-center justify-between mt-10">
+              <div>
+                <h2>Highlight Color</h2>
+              </div>
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
+                {colors
+                  ?.filter((item) => item.hex !== selectedThemeColor?.hex)
+                  .map((color, idx) => (
+                    <div
+                      key={`color_${idx}`}
+                      className={`border-2 ${
+                        color?.hex === selectedTheme?.highlight_color
+                          ? "border-blue-600"
+                          : "border-gray-300"
+                      } p-1 rounded-2xl`}
+                    >
+                      <span
+                        key={color.hex}
+                        className={`w-[50px] h-[50px] block rounded-xl`}
+                        style={{ backgroundColor: color?.hex }}
+                        onClick={() =>
+                          handleChooseThemeColor(color, "highlight")
+                        }
+                      ></span>
+                    </div>
+                  ))}
+              </div>
+            </div>
+          )}
+        </div>
+        {colors && (
+          <Button
+            variant="flat"
+            onPress={handleOpenMoreColorModal}
+            className="mt-8 border-gray-300 h-fit w-fit border border-2 border-gray-400 rounded-3xl flex items-center justify-center py-2 px-4 mx-auto"
+          >
+            <ProIcon name={"GoPlus"} size={20} color="#333" />
+            Add color
+          </Button>
         )}
       </div>
-     {colors && <Button variant="flat" onPress={handleOpenMoreColorModal} className="mt-8 border-gray-300 h-fit w-fit border border-2 border-gray-400 rounded-3xl flex items-center justify-center py-2 px-4 mx-auto">
-        <ProIcon name={'GoPlus'} size={20} color="#333" />Add color
-      </Button>}
-    </div>
-    <Modal isOpen={isOpen} size='md' onClose={handleOpenMoreColorModal}>
-      <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1 text-black">Add favorite color</ModalHeader>
-            <ModalBody>
-              <div className="flex items-center justify-center">
-              <HexColorPicker color={color} onChange={setColor} />
-              </div>
-            </ModalBody>
-            <ModalFooter>
-              <Button color="danger" variant="light" onPress={onClose}>
-                Close
-              </Button>
-              <Button color="primary" onPress={handleAddColor}>
-                Add color
-              </Button>
-            </ModalFooter>
-          </>
-        )}
-      </ModalContent>
-    </Modal>
-  </>
-}
+      <Modal isOpen={isOpen} size="md" onClose={handleOpenMoreColorModal}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 text-black">
+                Add favorite color
+              </ModalHeader>
+              <ModalBody>
+                <div className="flex items-center justify-center">
+                  <HexColorPicker color={color} onChange={setColor} />
+                </div>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={handleAddColor}>
+                  Add color
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
 
-export default ThemeSelector
+export default ThemeSelector;
--- src/DB/paattoholic_noufa.js
+++ src/DB/paattoholic_noufa.js
@@ -1,14 +1,16 @@
-import Bride from "../assets/coffeePremium/Ashmil-Munshida/bride.jpg";
-import image01 from "../assets/coffeePremium/Ashmil-Munshida/image01.jpg";
-import image02 from "../assets/coffeePremium/Ashmil-Munshida/image02.jpg";
-import image03 from "../assets/coffeePremium/Ashmil-Munshida/image03.jpg";
-import Groom from "../assets/coffeePremium/Ashmil-Munshida/groom.jpg";
-import image04 from "../assets/coffeePremium/Ashmil-Munshida/image04.jpg";
-import image05 from "../assets/coffeePremium/Ashmil-Munshida/image05.jpg";
+import Bride from "../assets/coffeePremium/paattoholic-noufa/bride.jpg";
+import image01 from "../assets/coffeePremium/paattoholic-noufa/patttholic1.jpg";
+import image02 from "../assets/coffeePremium/paattoholic-noufa/patttholic2.jpg";
+import image03 from "../assets/coffeePremium/paattoholic-noufa/patttholic3.jpg";
+import image04 from "../assets/coffeePremium/paattoholic-noufa/patttholic4.jpg";
+import image05 from "../assets/coffeePremium/paattoholic-noufa/patttholic8.jpg";
+import image06 from "../assets/coffeePremium/paattoholic-noufa/patttholic6.jpg";
+import image07 from "../assets/coffeePremium/paattoholic-noufa/patttholic7.jpg";
+import Groom from "../assets/coffeePremium/paattoholic-noufa/groom.jpg";
 
 export const paattoholic_noufa = {
   bride: "Noufa",
-  groom: "Paattoholic",
+  groom: "Ismail",
   phone1: "",
   theme: "#3c4028",
   default_color: "#FEF3E2",
@@ -16,32 +18,33 @@ export const paattoholic_noufa = {
   highlight_color: "#ddae6b",
   phone2: "",
   begin_time: "11:30 AM",
-  music: "/audio/audio1.mp3",
+  music: "/audio/arabic.mp3",
   quote:
     "invite you to join us as we celebrate the beginning of our forever. Surrounded by the blessings of family, friends, and well-wishers, we step into a new chapter of life together and your presence will make our day truly complete.",
   hide_info: false,
   couples_data: [
     {
-      full_name: "Paattoholic",
+      full_name: "Muhammad Ismail",
       bio: "S/O   Father name",
       avatar: Groom,
     },
     {
-      full_name: "Noufa",
+      full_name: "Fathima Noufa",
       bio: "D/O Father name",
       avatar: Bride,
     },
   ],
 
   images: [
-    image02,
     image01,
+    image02,
     image03,
     image04,
-    image02,
-    image05,
-    image01,
+    image06,
+    image07,
     image03,
+    image04,
+    image06,
   ],
   dateData: [
     { date: "22", day: "Thu" },
@@ -54,10 +57,10 @@ export const paattoholic_noufa = {
   ],
   month: "May",
   //   muhurtham: "11:50 AM",
-  venue: "NKM Convention Centre ",
-  place: "Ailasseri , Vellayur",
+  venue: "Home",
+  place: "Ramanatukara",
   date: "2025-05-25",
-  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4225.480738554696!2d76.29323797533296!3d11.146203552263684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba62f184bf81e1d%3A0xe81c7f41a36ed4b5!2sNKM%20Convention%20Centre!5e1!3m2!1sen!2sin!4v1745952661086!5m2!1sen!2sin",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30415.041781154996!2d75.85037473366121!3d11.187634142225576!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba65a835c8c029b%3A0x32bb8b5df6f3194a!2sRamanattukara%2C%20Kerala!5e1!3m2!1sen!2sin!4v1748532182414!5m2!1sen!2sin",
   // map_name: "Kalikavu",
-  map_link: "https://maps.app.goo.gl/8rnFL2tsYoqv3e9K8",
+  map_link: "https://maps.app.goo.gl/JJZSi8P5yeQ2Lm4HA",
 };
