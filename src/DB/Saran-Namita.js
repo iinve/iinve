4096db3e27ff5e68a278edb81d92d4e9091fbaf7
dilshanmambadan <dilshanmambadan@gmail.com>
2025-07-26 01:59:07 +0530

    create saran

---
+++ src/DB/Saran-Namita.js
@@ -0,0 +1,88 @@
+import Bride from "../assets/coffeePremium/Karthik-Varsha/bride_avathar.jpeg";
+import image08 from "../assets/coffeePremium/Karthik-Varsha/eight.jpeg";
+import image05 from "../assets/coffeePremium/Karthik-Varsha/five.jpeg";
+import image04 from "../assets/coffeePremium/Karthik-Varsha/four.jpeg";
+import Groom from "../assets/coffeePremium/Karthik-Varsha/groom_avathar.jpeg";
+import image01 from "../assets/coffeePremium/Karthik-Varsha/one.jpeg";
+import image07 from "../assets/coffeePremium/Karthik-Varsha/seven.jpeg";
+import image06 from "../assets/coffeePremium/Karthik-Varsha/six.jpeg";
+import image03 from "../assets/coffeePremium/Karthik-Varsha/three.jpeg";
+import image02 from "../assets/coffeePremium/Karthik-Varsha/two.jpeg";
+
+export const karthik_varsha = {
+  groom: "Saran",
+  bride: "Varsha",
+  phone1: "",
+  theme: "#3c4028",
+  default_color: "#FEF3E2",
+  highlight_color: "#ddae6b",
+  phone2: "",
+  begin_time: "10:30 AM",
+  reception_begin_time: "05:00 pM",
+  music: "/audio/sita_kalyanam.mp3",
+  quote:
+    "You are also invited for the post-marriage reception that will be arranged at Travancore International Convention Center (The Sports hub, Green Field international Stadium) Karyavattom, on <b> 13th july 2025 Sunday from 5:00pm to 10:00pm </b>",
+  wdn_quote:
+    "Cordially invite your esteemed presence with family on the auspicious occasion of our wedding ceremony . ",
+  location_name: "Reception Location ",
+  hide_info: false,
+  invite_quote:
+    "With hearts full of love and joy, we <b>Saran & Namitha</b>, invite you our beloved family to join us in celebrating our union and the beginning of our new life together. Your presence at our wedding ceremony and reception will mean the world to us as we embark on this beautiful journey as a family. <br/><br/> Let us come together to share in the love, happiness and cherished memories on <b>11-July-2025 </b> at <b>Safa Convention Centre </b> and Thonnakkal , Mangalapuram. We look forward to creating unforgettable moments with all of you.",
+  couples_data: [
+    {
+      full_name: "Saran",
+      bio: "S/O Saraswathy.C & Sivakumar",
+      avatar: Groom,
+    },
+    {
+      full_name: "Namitha",
+      bio: "D/O Jayasree.C & NG Rameshan Nair",
+      avatar: Bride,
+    },
+  ],
+
+  images: [
+    image06,
+    image03,
+    image04,
+    image02,
+    image05,
+    image01,
+    image08,
+    image07,
+  ],
+  dateData: [
+    { date: "08", day: "Tue" },
+    { date: "09", day: "Wed" },
+    { date: "10", day: "Thu" },
+    { date: "11", day: "Fri", active: true },
+    { date: "12", day: "Sat" },
+    { date: "13", day: "Sun" },
+    { date: "14", day: "Mon" },
+  ],
+  dateReception: [
+    { date: "10", day: "Thu" },
+    { date: "11", day: "Fri" },
+    { date: "12", day: "Sat" },
+    { date: "13", day: "Sun", active: true },
+    { date: "14", day: "Mon" },
+    { date: "15", day: "Tue" },
+    { date: "16", day: "Wed" },
+  ],
+  month: "Aug",
+  muhurtham: `<b> 12:00 </b> & <b> 12:20 </b>`,
+  reception_muhurtham: `<b> 05:00 </b> & <b> 10:00 </b>`,
+  venue: "Grace Auditorium",
+  place: "Kodukulanji , Chengannur",
+  date: "2025-08-28",
+  reception_venue: "RDR convention centre",
+  reception_place: "Edapazhanji , Trivandrum",
+  reception_date: "2025-08-30",
+  short_date: "28-Aug-2025",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4257.938559518577!2d76.84405947531285!3d8.631641195081729!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05c03fb0049aef%3A0x843c0c1f0b9a877f!2sSafa%20Convention%20Centre!5e1!3m2!1sen!2sin!4v1748358184714!5m2!1sen!2sin",
+  map_reception:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3832.2250118127654!2d76.88546939999999!3d8.5729369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b05bf1b75dfbd09%3A0x195da672c6f81df1!2sTravancore%20International%20Convention%20Centre!5e1!3m2!1sen!2sin!4v1751286921750!5m2!1sen!2sin",
+  // map_name: "Kalikavu",
+  map_link: "https://maps.app.goo.gl/gVM35tUa1b7FLZtE9",
+  reception_link: "https://maps.app.goo.gl/nsM1eDCCZXemXBTD6",
+};
