0711d810f8ea12d40c0e7457a49351d3f81446ef
muhammeddilshan <dilshanmambadan@gmail.com>
2025-08-06 14:10:55 +0530

    najiya invitation created

---
+++ src/DB/shamil-najiya.js
@@ -0,0 +1,54 @@
+import image01 from "../assets/coffeePremium/shamil-najiya/img1.jpg";
+import image02 from "../assets/coffeePremium/shamil-najiya/img2.jpg";
+import image03 from "../assets/coffeePremium/shamil-najiya/img3.jpg";
+import Bride from "../assets/coffeePremium/shamil-najiya/bride.jpg";
+import Groom from "../assets/coffeePremium/shamil-najiya/groom.jpg";
+
+export const shamil_najiya = {
+  bride: "Najiya",
+  groom: "Shamil",
+  phone1: "",
+  theme: "#3c4028",
+  default_color: "#FEF3E2",
+  send_wishes: false,
+  event_type: "wedding",
+  highlight_color: "#ddae6b",
+  phone2: "",
+  begin_time: "11:00 AM",
+  music: "/audio/tamil_theme.mp3",
+  quote:
+    "invite you to join us as we celebrate the beginning of our forever. Surrounded by the blessings of family, friends, and well-wishers, we step into a new chapter of life together and your presence will make our day truly complete.",
+  hide_info: false,
+  couples_data: [
+    {
+      full_name: "Shamil",
+      bio: "S/o  father name",
+      avatar: Groom,
+    },
+    {
+      full_name: "Najiya",
+      bio: "D/o Firos & Jaseena",
+      avatar: Bride,
+    },
+  ],
+
+  images: [image01, image02, image03, image01, image02, image03],
+
+  dateData: [
+    { date: "04", day: "Thu" },
+    { date: "05", day: "Fri" },
+    { date: "06", day: "Sat" },
+    { date: "07", day: "Sun", active: true },
+    { date: "08", day: "Mon" },
+    { date: "09", day: "Tue" },
+    { date: "10", day: "Wed" },
+  ],
+  month: "Sep",
+  //   muhurtham: "11:50 AM",
+  venue: "BB Auditorium",
+  place: "Kalikavu",
+  date: "2025-08-07",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3802.1301583525865!2d76.32782651046787!3d11.168573688959235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba62f1b66b730d3%3A0xded50677f568a2af!2sBB%20Auditorium%20%26%20Panjami%20Annex!5e1!3m2!1sen!2sin!4v1754467471628!5m2!1sen!2sin",
+  // map_name: "Kalikavu",
+  map_link: "https://maps.app.goo.gl/4s15wiZo5EXyFgMn8",
+};
