ca8744b8f90666e880bc628e837b61131d317000
muhammeddilshan <dilshanmambadan@gmail.com>
2025-08-26 15:56:54 +0530

    shamil invitation updated

---
+++ src/DB/shamilhouse.js
@@ -0,0 +1,57 @@
+import image01 from "../assets/coffeePremium/shamil-najiya/s1.jpg";
+import image02 from "../assets/coffeePremium/shamil-najiya/s2.jpg";
+import image03 from "../assets/coffeePremium/shamil-najiya/s3.jpg";
+import image04 from "../assets/coffeePremium/shamil-najiya/n1.jpg";
+import image05 from "../assets/coffeePremium/shamil-najiya/n2.jpg";
+import image06 from "../assets/coffeePremium/shamil-najiya/n3.jpg";
+import Bride from "../assets/coffeePremium/shamil-najiya/bride.jpg";
+import Groom from "../assets/coffeePremium/shamil-najiya/groom.jpg";
+
+export const shamilhouse = {
+  bride: "Najiya",
+  groom: "Shamil",
+  phone1: "",
+  theme: "#3c4028",
+  default_color: "#FEF3E2",
+  send_wishes: false,
+  event_type: "wedding",
+  highlight_color: "#ddae6b",
+  phone2: "",
+  begin_time: "11:00 AM",
+  music: "/audio/tamil_theme.mp3",
+  quote:
+    "invite you to join us as we celebrate the beginning of our forever. Surrounded by the blessings of family, friends, and well-wishers, we step into a new chapter of life together and your presence will make our day truly complete.",
+  hide_info: false,
+  couples_data: [
+    {
+      full_name: "Shamil",
+      bio: "S/o  Saidali & Sajitha",
+      avatar: Groom,
+    },
+    {
+      full_name: "Najiya",
+      bio: "D/o Firos & Jaseena",
+      avatar: Bride,
+    },
+  ],
+
+  images: [image01, image04, image02, image05, image03, image06],
+
+  dateData: [
+    { date: "08", day: "Mon" },
+    { date: "09", day: "Tue" },
+    { date: "10", day: "Wed" },
+    { date: "11", day: "Thu", active: true },
+    { date: "12", day: "Fri" },
+    { date: "13", day: "Sat" },
+    { date: "14", day: "Sun" },
+  ],
+  month: "Sep",
+  //   muhurtham: "11:50 AM",
+  venue: "N K M auditorium",
+  place: "Ailasseri",
+  date: "2025-08-11",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3802.442480766999!2d76.29719779999999!3d11.1447103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba62f184bf81e1d%3A0xe81c7f41a36ed4b5!2sNKM%20Convention%20Centre!5e1!3m2!1sen!2sin!4v1756203779812!5m2!1sen!2sin",
+  // map_name: "Kalikavu",
+  map_link: "https://maps.app.goo.gl/z8v4ZQKHdn21s1gFA",
+};
