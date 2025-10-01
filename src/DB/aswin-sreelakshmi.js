8d2d0099f95ea86e90f9cd26f8111985fd2f12bf
muhammeddilshan <dilshanmambadan@gmail.com>
2025-10-01 12:00:39 +0530

    add aswin

---
+++ src/DB/aswin-sreelakshmi.js
@@ -0,0 +1,86 @@
+import Bride from "../assets/coffeePremium/aswin-sreelakshmi/bride.png";
+import Groom from "../assets/coffeePremium/aswin-sreelakshmi/groom.png";
+import image01 from "../assets/coffeePremium/aswin-sreelakshmi/img1.jpg";
+import image02 from "../assets/coffeePremium/aswin-sreelakshmi/img2.jpg";
+import image03 from "../assets/coffeePremium/aswin-sreelakshmi/img3.jpg";
+import image04 from "../assets/coffeePremium/aswin-sreelakshmi/img4.jpg";
+import image05 from "../assets/coffeePremium/aswin-sreelakshmi/img5.jpg";
+import image06 from "../assets/coffeePremium/aswin-sreelakshmi/img6.jpg";
+
+export const aswin_sreelakshmi = {
+  groom: "Aswin",
+  bride: "Sreelakshmi",
+  phone1: "",
+  theme: "#b7bdb0",
+  default_color: "#333",
+  highlight_color: "#014a26ff",
+  phone2: "",
+  begin_time: "06:00 AM",
+  reception_begin_time: "04:00 PM",
+  music: "/audio/sitakalyanam_Song.mp3",
+  quote:
+    "You are also invited for the post-marriage reception that will be arranged at RDR convention centre  Edapazhanji , Trivandrum, on <b> 30th August  2025 Saturday from 5:00pm to 09:00pm </b>",
+  wdn_quote:
+    "Cordially invite your esteemed presence with family on the auspicious occasion of our wedding ceremony . ",
+  location_name: "Reception Location ",
+  hide_info: false,
+  invite_quote:
+    "With hearts full of love and joy, we, <b>Aswin & Sreelakshmi</b>, warmly invite you, our beloved family and friends, to join us in celebrating our union and the beginning of our new life together. Your presence at our wedding ceremony and reception would mean the world to us as we embark on this beautiful journey as one. Let us gather to share in the love, happiness, and cherished memories on <b>28th August 2025</b>, at <b>Grace Auditorium</b>, Kodukulanji, Chengannur. We look forward to creating unforgettable moments with each and every one of you.",
+  couples_data: [
+    {
+      full_name: "Aswin",
+      bio: "S/O Sathyan & Asha",
+      avatar: Groom,
+    },
+    {
+      full_name: "Sreelakshmi",
+      bio: "D/O Santhosh & Shibini",
+      avatar: Bride,
+    },
+  ],
+
+  images: [
+    image02,
+    image01,
+    image04,
+    image06,
+    image02,
+    image05,
+    image01,
+    image03,
+  ],
+  dateData: [
+    { date: "12", day: "Wed" },
+    { date: "13", day: "Thu" },
+    { date: "14", day: "Fri" },
+    { date: "15", day: "Sat", active: true },
+    { date: "16", day: "Sun" },
+    { date: "17", day: "Mon" },
+    { date: "18", day: "Tue" },
+  ],
+  dateReception: [
+    { date: "12", day: "Wed" },
+    { date: "13", day: "Thu" },
+    { date: "14", day: "Fri" },
+    { date: "15", day: "Sat", active: true },
+    { date: "16", day: "Sun" },
+    { date: "17", day: "Mon" },
+    { date: "18", day: "Tue" },
+  ],
+  month: "Nov",
+  muhurtham: `<b> 06:00 </b> & <b> 06:30 </b>`,
+  reception_muhurtham: `<b> 04:00 </b> & <b> 07:00 </b>`,
+  venue: "Guruvayur Temple",
+  place: "Guruvayur ",
+  date: "2025-11-15",
+  reception_venue: "Sampathikodharana Sangham Hall",
+  reception_place: "Elavally ",
+  reception_date: "2025-11-15",
+  short_date: "15-Nov-2025",
+  map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3809.458177125754!2d76.03684631045931!3d10.594691389499644!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba79461d5c8d24b%3A0x6f7a8d815f48709e!2sGuruvayur%20Shri%20Krishna%20Temple!5e1!3m2!1sen!2sin!4v1759299772502!5m2!1sen!2sin",
+  map_reception:
+    "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3809.7846159108117!2d76.09009361045891!3d10.568410589524476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7ecae167e3a11%3A0x49832815a5eee!2sSampathikodharana%20Sangham%20Hall!5e1!3m2!1sen!2sin!4v1759299607198!5m2!1sen!2sin",
+  // map_name: "Kalikavu",
+  map_link: "https://maps.app.goo.gl/UbFaYBqkV1row4cC7",
+  reception_link: "https://maps.app.goo.gl/4zDUARFU8EEYZxBT6",
+};
