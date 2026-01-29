47dcde59939ecff9f7a58b25d22deef79d0465b4
hananpilakkadan <hananpilakkadan@gmail.com>
2026-01-29 21:40:10 +0530

    remove rout

--- src/app/invite/sreerag-krishnapriya/page.js
+++ src/app/invite/sreerag-krishnapriya/page.js
@@ -47,7 +47,7 @@ const Page = () => {
           />
         )}
       </Head>
-      <BasicEternity data={sreerag_krishnapriya_} />
+      {/* <BasicEternity data={sreerag_krishnapriya_} /> */}
     </div>
   );
 };
